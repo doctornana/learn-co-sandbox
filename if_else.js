05ee568f3a7b6f99496c2f4c8098f4074f037fa2
Nasiha Ocasio <ocasin01@gmail.com>
2018-08-13 22:07:46 +0000

    automatically backed up by learn

---
+++ if_else.js
@@ -0,0 +1,18 @@
+function sayHelloToIsabel() {
+  console.log("Hello, Isabel!")
+}
+function sayHelloToSofia() {
+  console.log("Hello, Sofia!")
+}
+function sayHelloToBrendan() {
+  console.log("Hello, Brendan!")
+}
+function sayHelloTo(firstName) { console.log("I didn't see you there.")
+  return (`Hello, ${firstName}!`)
+}
+say("Hello", "Sofia!")
+
+
+function add(x,y) {console.log (x + y)
+  return (x + y)}
+
